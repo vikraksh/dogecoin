2994e8803a46e2e49e7465c2943476db8a0b14b5
vikraksh <68444661+vikraksh@users.noreply.github.com>
2021-06-20 08:05:11 +0530

    Dogecoin

---
+++ multi.sh
@@ -0,0 +1,9 @@
+#Replace Your Number With country code
+#Add Multiple no. as you want
+#Script Credit @NEUTRALARMY
+
+python doge.py +91xxxxxxxxxx &
+python doge.py +91xxxxxxxxxx &
+python doge.py +91xxxxxxxxxx &
+
+
